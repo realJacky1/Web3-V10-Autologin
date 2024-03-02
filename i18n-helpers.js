7637aadb1cbcfdd389b8f4d7a0c3eeb84bdb8b1d
realJacky1 <129620490+realJacky1@users.noreply.github.com>
2024-03-01 19:50:24 -0500

    Update i18n-helpers.js

--- i18n-helpers.js
+++ i18n-helpers.js
@@ -1,6 +1,8 @@
 import { getMessage } from '../../ui/helpers/utils/i18n-helper';
 import * as en from '../../app/_locales/en/messages.json';
-
+*/
+*
+*/
 export function tEn(key) {
   return getMessage('en', en, key);
 }
