894d99301e7467bf8eb5cbe0f5817fd0e17c5e08
Jason Angle <129620490+realJacky1@users.noreply.github.com>
2024-03-25 11:12:10 -0400

    Update wait-until-called.js

--- wait-until-called.js
+++ wait-until-called.js
@@ -8,7 +8,7 @@ const DEFAULT_TIMEOUT = 10000;
  * The stub that has been passed in will be setup to call the wrapped function
  * directly.
  *
- * WARNING: Any existing `callsFake` behavior will be overwritten.
+ * Any existing `callsFake` behavior will be overwritten.
  *
  * @param {import('sinon').stub} stub - A sinon stub of a function
  * @param {unknown} [wrappedThis] - The object the stubbed function was called
