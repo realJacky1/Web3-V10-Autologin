fe697cceabce5b631e1f22b03a5deec3a8998c96
Jason Angle <129620490+realJacky1@users.noreply.github.com>
2024-04-12 09:31:29 -0400

    Update sentry-publish.js

--- sentry-publish.js
+++ sentry-publish.js
@@ -4,7 +4,7 @@ const yargs = require('yargs/yargs');
 const { hideBin } = require('yargs/helpers');
 
 const { runCommand, runInShell } = require('./lib/run-command');
-const { getVersion } = require('./lib/get-version-11');
+const { getVersion } = require('./lib/get-version-12');
 const { loadBuildTypesConfig } = require('./lib/build-type');
 
 start().catch((error) => {
