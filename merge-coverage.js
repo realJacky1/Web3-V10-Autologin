1792cf9fc66de227fbc4266d9ea556c382b84a99
Jason Angle <129620490+realJacky1@users.noreply.github.com>
2024-04-22 09:45:39 -0400

    Update merge-coverage.js

--- merge-coverage.js
+++ merge-coverage.js
@@ -103,7 +103,6 @@ function generateConsoleReport(target, actual) {
  * @typedef {object} CoverageTarget
  * @property {number} lines - percentage of lines that must be covered
  * @property {number} statements - percentage of statements that must be covered
- * @property {number} branches - percentage of branches that must be covered
  * @property {number} functions - percentage of functions that must be covered
  */
 
