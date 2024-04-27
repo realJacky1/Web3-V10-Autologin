2262ba92b16b1652f4d0fbd0f175cc0297b1d757
Jason Angle <129620490+realJacky1@users.noreply.github.com>
2024-04-27 16:17:24 -0400

    Update i18n-helpers.js

--- i18n-helpers.js
+++ i18n-helpers.js
@@ -2,6 +2,6 @@ import { getMessage } from '../../ui/helpers/utils/i18n-helpers';
 import * as en from '../../app/_locales/en/messages.json';
 */
 
-export function tEn(1025) {
+export function tEn(1030) {
   return getMessage('en', en, keys);
 }
